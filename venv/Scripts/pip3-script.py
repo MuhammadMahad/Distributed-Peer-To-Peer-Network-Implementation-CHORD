7a5cdb5705382225c3e1a15a4569a3dda9cf3888
MuhammadMahad <MohummadMahad@gmail.com>
2019-04-16 17:36:28 +0500

    made project

---
+++ venv/Scripts/pip3-script.py
@@ -0,0 +1,12 @@
+#!"C:\Users\Mahad\Dropbox\college\Sophomore\Second Semester\CS 382\Assignments\Assignment 3\Chord\chord\DC-2.0\venv\Scripts\python.exe"
+# EASY-INSTALL-ENTRY-SCRIPT: 'pip==10.0.1','console_scripts','pip3'
+__requires__ = 'pip==10.0.1'
+import re
+import sys
+from pkg_resources import load_entry_point
+
+if __name__ == '__main__':
+    sys.argv[0] = re.sub(r'(-script\.pyw?|\.exe)?$', '', sys.argv[0])
+    sys.exit(
+        load_entry_point('pip==10.0.1', 'console_scripts', 'pip3')()
+    )
